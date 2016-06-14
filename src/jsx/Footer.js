d5b9505817ffaa47340deebb25d17ab04c034938
e <970231732@qq.com>
2016-06-14 14:58:34 +0800

    博客底部的实现

---
+++ src/jsx/Footer.js
@@ -0,0 +1,12 @@
+var Footer=React.createClass({
+	render:function(){
+		return(<div className="footer">
+			   
+			    <ul className="footer_con">
+			     <li><i class="fa fa-phone" aria-hidden="true"></i>Tel:15898519690</li>
+			     <li><i class="fa fa-qq" aria-hidden="true"></i>QQ:970231732</li>
+			     <li><i class="fa fa-github" aria-hidden="true"></i><a href="https://github.com/xuemeixu">project:git@github.com:xuemeixu</a></li>
+			    </ul>
+			   </div>);
+	}
+})
