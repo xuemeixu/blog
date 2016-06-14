9e819478be42eb54e0d1501aaf21a15ba54f48e7
e <970231732@qq.com>
2016-06-14 14:55:16 +0800

    react实现文章的总体

---
+++ src/jsx/Article.js
@@ -0,0 +1,27 @@
+var ARTICLE=[
+		{
+			id:"1",
+			title:"Linux内核编译",	
+			tag:"其他",
+			date:"2016-05-07",
+			description:"Linux内核编译",
+			content:Article1
+       },
+       {
+			id:"2",
+			title:"JavaScript的基本数据类型",	
+			tag:"JavaScript",
+			date:"2016-05-07",
+			description:"js中一些数据类型的总结",
+			content:Article2
+       },
+        {
+			id:"3",
+			title:"前端之路",	
+			tag:"随笔",
+			date:"2016-05-28",
+			description:"一些学习前端的心得体会",
+			content:Article3
+       },
+      
+       ];
